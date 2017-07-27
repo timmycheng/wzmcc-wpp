585db9bd57d8e15637cb95ab34da67601770be93
timmycheng <timmycheng@foxmail.com>
2017-07-27 19:19:22 +0800

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+// 声明
+var express = require('express')
+var path = require('path')
+var port = process.env.PORT || 3000
+var app = express()
+
+// 设置
+app.set('views', './views')
+app.set('view engine', 'jade')
+app.use(express.static(path.join(__dirname, 'bower_components')))
+
+// 启动服务
+app.listen(port)
+console.log('start web server at ', port, '...')
+
+
+// routes
+// index: /
+
+
+app.get('/', function(req, res){
+	console.log('get')
+	res.send('hello world.')
+})
